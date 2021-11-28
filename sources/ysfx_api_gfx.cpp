75dfc23fe7d73392f38af413c02ea1915cf8eea3
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-11-28 11:07:19 +0100

    Reinitialize more gfx variables

--- sources/ysfx_api_gfx.cpp
+++ sources/ysfx_api_gfx.cpp
@@ -218,7 +218,12 @@ void ysfx_gfx_enter(ysfx_t *fx, bool doinit)
             *fx->var.gfx_dest = -1.0;
             *fx->var.mouse_wheel = 0.0;
             *fx->var.mouse_hwheel = 0.0;
-            // NOTE possibly others in the future, check eel_lice.h `resetVarsToStock`
+            // NOTE the above are according to eel_lice.h `resetVarsToStock`
+            //   it helps to reset a few more, especially for clearing
+            *fx->var.gfx_mode = 0;
+            *fx->var.gfx_clear = 0;
+            *fx->var.gfx_texth = 0;
+            *fx->var.mouse_cap = 0;
 
             // reset key state
             state->input_queue = {};
