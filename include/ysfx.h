a36fac12f4d7d7a6328d50c04242202bb0f0b7a1
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-12-03 16:51:43 +0100

    Minor change of API

--- include/ysfx.h
+++ include/ysfx.h
@@ -333,7 +333,7 @@ typedef struct ysfx_gfx_config_s {
     // the scale factor of the display; 1.0 or greater, 2.0 for Retina display
     ysfx_real scale_factor;
     // show a menu and run it synchronouslys; returns an item ID >= 1, or 0 if none
-    int (*show_menu)(void *user_data, const char *menu_spec, int32_t xpos, int32_t ypos);
+    int32_t (*show_menu)(void *user_data, const char *menu_spec, int32_t xpos, int32_t ypos);
     // change the cursor
     void (*set_cursor)(void *user_data, int32_t cursor);
 } ysfx_gfx_config_t;
